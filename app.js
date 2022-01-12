3b9b52c22b55dca27fff231ede0cbcc1747498c7
Elena Tsoy <ltsoy1120@gmail.com>
2022-01-12 10:40:27 +0600

    added gitignore

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const PORT = config.get("port") || 5000;
+
+const app = express();
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true
+    });
+    app.listen(PORT, () => {
+      console.log(`App has been started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
